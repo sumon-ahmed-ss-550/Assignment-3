3a48116ed8846199e5726102a3760680ee48135c
sumon550 <entertainmentcharacter54321@gmail.com>
2026-02-08 13:07:06 +0600

    Add problem-1 task

--- final.js
+++ final.js
@@ -1,1 +1,15 @@
-
+// Problem-1
+function newPrice(currentPrice, discount) {
+  if (typeof currentPrice !== "number" || typeof discount !== "number") {
+    return "Invalid";
+  } else {
+    if (discount < 0 || discount > 100) {
+      return "Invalid";
+    } else {
+      const discountPrice = (currentPrice * discount) / 100;
+      let price = currentPrice - discountPrice;
+      price = price.toFixed(3);
+      return price;
+    }
+  }
+}
